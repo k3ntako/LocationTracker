e8a74dc6f1e0b2df46e0812169111cf064da11f9
k3ntako <kentarokaneki@gmail.com>
2019-10-22 23:37:52 -0400

    Install React Native Maps

--- ios/LocationTracker/AppDelegate.m
+++ ios/LocationTracker/AppDelegate.m
@@ -11,10 +11,13 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyAAapITOX3MSLV-0SaQSnIf2UYIAb3UUZE"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"LocationTracker"
